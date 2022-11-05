bc2d6fbdbc260b5082322cbae8d2877b51e4094a
Seongyeon Lee <winkite1226@gmail.com>
2022-11-06 02:06:06 +0900

    modify media file name

--- drf_prac_back/settings.py
+++ drf_prac_back/settings.py
@@ -169,7 +169,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mdeia"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
