fed364c0f7957743ec96c4c63c2fa3de84031fba
Seongyeon Lee <winkite1226@gmail.com>
2022-11-06 00:32:24 +0900

    create user, article, comment models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Article(models.Model):
+    class Meta:
+        db_table = 'article'
+
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_title = models.CharField(max_length=50)
+    post_restaurant = models.CharField(max_length=30)
+    post_content = models.TextField()
+    post_image = models.ImageField(blank=True, upload_to="$Y/%m")
+    post_like = models.ManyToManyField(User, related_name="like_articles")
+    post_created = models.DateTimeField(auto_now_add=True)
+    post_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.post_title)
+
+
+class Comment(models.Model):
+    class Meta:
+        db_table = 'comment'
+
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_postid = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comment_set")
+    comment_content = models.TextField()
+    comment_created = models.DateTimeField(auto_now_add=True)
+    comment_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.comment_content)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Meta:
+            db_table = 'user'
+
+    # id : pk
+    # username : 아이디
+    # email : 이메일
+    # date_joined : 계정이 생성된 날짜
+    phone = models.IntegerField(default=0)
+    nickname = models.CharField(max_length=30)
+    profile_img = models.ImageField(blank=True, upload_to="$Y/%m")
+    introduce = models.CharField(max_length=50)
